ca1d11fbba360d4cc4c2c53e40f618c87ec7ef17
DaegeunChoe <asdf12gg@gmail.com>
2025-07-08 15:58:45 +0900

    아주 아주 간단한 툰 셰이더를 적용한 캐릭터 에셋 임포트

--- Source/Homework7/Private/MyPawn.cpp
+++ Source/Homework7/Private/MyPawn.cpp
@@ -1,9 +1,24 @@
 #include "MyPawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 AMyPawn::AMyPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	Collision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collision"));
+	SetRootComponent(Collision);
+
+	SkeletalMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMeshComp->SetupAttachment(Collision);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(Collision);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 void AMyPawn::BeginPlay()
--- Source/Homework7/Public/MyPawn.h
+++ Source/Homework7/Public/MyPawn.h
@@ -4,6 +4,11 @@
 #include "GameFramework/Pawn.h"
 #include "MyPawn.generated.h"
 
+class UCapsuleComponent;
+class USkeletalMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class HOMEWORK7_API AMyPawn : public APawn
 {
@@ -13,6 +18,18 @@ public:
 	AMyPawn();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCapsuleComponent> Collision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCameraComponent> Camera;
 
 protected:
 	virtual void BeginPlay() override;
